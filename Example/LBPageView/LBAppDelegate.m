0315d4a7d74016450af446ccf91e3c5f53845d7e
SYXGirl <1553380991@qq.com>
2018-08-01 16:05:58 +0800

    asdfasdf

--- Example/LBPageView/LBAppDelegate.m
+++ Example/LBPageView/LBAppDelegate.m
@@ -13,7 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
-
+//asdfasfasfasfsa
     return YES;
 }
 
